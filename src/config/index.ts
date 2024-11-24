10416dd14515a11e975a1dce494c127fb987032a
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2024-11-24 16:25:47 -0600

    adapters

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,5 @@
-export * from './envs';
+export * from './adapters/envs';
+export * from './adapters/bcrypt';
+export * from './adapters/jwt';
+
 export * from './validators';
-export * from './bcrypt';
-export * from './jwt';
