9d0036d2c077cb35c53ecb21eb52229894aec09d
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2024-11-24 17:13:11 -0600

    use uuid for short id

--- src/config/adapters/short-id.ts
+++ src/config/adapters/short-id.ts
@@ -1,7 +1,7 @@
-import { nanoid } from 'nanoid';
+import { v4 as uuidv4 } from 'uuid';
 
 export class ShortIdAdapter {
   static generateShortId(): string {
-    return nanoid(8);
+    return uuidv4().slice(0, 8);
   }
 }
