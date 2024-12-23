1aca4a9f4a6878204928bf5f46f58eca5f652a78
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2024-12-22 21:17:33 -0700

    remove dist

--- dist/config/adapters/date.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DateAdapter = void 0;
-class DateAdapter {
-    static now() {
-        return new Date();
-    }
-}
-exports.DateAdapter = DateAdapter;
