8dfcb82a396d824245dad5ba8c198e070a638465
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2024-12-25 19:52:28 -0700

    cookies dev config

--- src/config/adapters/cookie.ts
+++ src/config/adapters/cookie.ts
@@ -11,7 +11,7 @@ export class CookieAdapter {
     return {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'none',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       domain: process.env.NODE_ENV === 'production' ? '.cliplink.app' : undefined,
       maxAge: maxAge || 1000 * 60 * 60 * 24 * 30, // Default: 1 month
     };
@@ -20,7 +20,7 @@ export class CookieAdapter {
     return {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'none',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       domain: process.env.NODE_ENV === 'production' ? '.cliplink.app' : undefined
     };
   }
