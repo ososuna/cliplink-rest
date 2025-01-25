67a1ff643cacc89503085adefe19ccfa5cb26468
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2025-01-25 13:16:20 -0700

    refactor: reorganize test utilities and migrate mocks to a new directory structure

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    setupFiles: './unittest/test-utils/setup.ts',
+    setupFiles: './test/test-utils/setup.ts',
     coverage: {
       provider: 'v8'
     },
