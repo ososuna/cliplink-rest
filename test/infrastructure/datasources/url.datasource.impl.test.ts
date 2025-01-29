4c8e5ffef741ff68a76e18a692260e562befd60f
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2025-01-28 20:14:47 -0700

    test: add error handling tests for getUrl method in UrlDataSourceImpl

--- test/infrastructure/datasources/url.datasource.impl.test.ts
+++ test/infrastructure/datasources/url.datasource.impl.test.ts
@@ -2,6 +2,7 @@ import { beforeAll, beforeEach, describe, expect, it, vi } from 'vitest';
 import { UrlDataSourceMocks } from '../../test-utils/infrastructure/datasources/url.datasource.mocks';
 import { UrlDataSourceImpl } from '../../../src/infrastructure';
 import { UrlModel } from '../../../src/data/mongodb';
+import { Messages } from '../../../src/config';
 
 UrlDataSourceMocks.setupMocks();
 
@@ -26,6 +27,21 @@ describe('UrlDataSourceImpl', () => {
       expect(UrlModel.findById).toHaveBeenCalledTimes(1);
       expect(UrlModel.findById).toHaveBeenCalledWith('urlId');
     });
+
+    it('should throw not found error', async () => {
+      asMock(UrlModel.findById).mockResolvedValue(null);
+      await expect(urlDataSource.getUrl('urlId')).rejects.toThrow(Messages.URL_NOT_FOUND);
+      expect(UrlModel.findById).toHaveBeenCalledTimes(1);
+      expect(UrlModel.findById).toHaveBeenCalledWith('urlId');
+    });
+
+    it('should throw internal server error', async () => {
+      asMock(UrlModel.findById).mockRejectedValue(new Error('error'));
+      await expect(urlDataSource.getUrl('urlId')).rejects.toThrow(Messages.INTERNAL_SERVER_ERROR);
+      expect(UrlModel.findById).toHaveBeenCalledTimes(1);
+      expect(UrlModel.findById).toHaveBeenCalledWith('urlId');
+    });
+
   });
 
 });
