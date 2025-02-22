65d96ab9fc0b835aabcd69c53f8524992960494e
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2025-02-22 17:08:52 -0600

    test: add unit tests for getUrls, delete, getUrl, update, and getUrlByShortId methods in UrlRepositoryImpl

--- test/infrastructure/repositories/url.repository.impl.test.ts
+++ test/infrastructure/repositories/url.repository.impl.test.ts
@@ -2,6 +2,8 @@ import { beforeAll, describe, expect, it, vi } from 'vitest';
 import { UrlDataSourceMocks } from '../../test-utils/infrastructure/datasources/url.datasource.mocks';
 import { CreateUrlDto, Url, type UrlRepository } from '../../../src/domain';
 import { UrlDataSourceImpl, UrlRepositoryImpl } from '../../../src/infrastructure';
+import { asMock } from '../../test-utils/test-utils';
+import { UrlModel } from '../../../src/data/mongodb';
 
 UrlDataSourceMocks.setupMocks();
 
@@ -28,6 +30,65 @@ describe('UrlRepositoryImpl', () => {
       originalUrl: 'originalUrl',
       shortId: 'shortId',
       user: 'userId',
+    });
+  });
+
+  it('get urls', async () => {
+    const urlsPage = await urlRepository.getUrls('userId', 1, 9, '');
+    expect(urlsPage).instanceOf(Object);
+    expect(urlsPage.items).toEqual(UrlDataSourceMocks.urls);
+    expect(urlsPage.total).toBe(3);
+    expect(urlsPage.limit).toBe(9);
+    expect(urlsPage.page).toBe(1);
+  });
+
+  it('delete', async () => {
+    const url = await urlRepository.delete('urlId');
+    expect(url).instanceOf(Url);
+    expect(url).toEqual(UrlDataSourceMocks.url);
+  });
+
+  it('get url', async () => {
+    const url = await urlRepository.getUrl('urlId');
+    expect(url).instanceOf(Url);
+    expect(url).toEqual(UrlDataSourceMocks.url);
+  });
+
+  it('update', async () => {
+    asMock(UrlModel.findOne).mockResolvedValue(null);
+    const [_error, updateUrlDto] = CreateUrlDto.create({
+      name: 'newName',
+      originalUrl: 'newOriginalUrl'
+    });
+    const url = await urlRepository.update('urlId', 'userId', updateUrlDto!);
+    expect(url).instanceOf(Url);
+    expect(url).toEqual({
+      id: 'urlId',
+      name: 'newName',
+      originalUrl: 'originalUrl',
+      shortId: 'shortId',
+      user: 'userId',
+    });
+  });
+
+  it('get url by short id', async () => {
+    asMock(UrlModel.findOne).mockResolvedValue({
+      _id: 'urlId',
+      originalUrl: 'originalUrl',
+      shortId: 'shortId',
+      user: 'userId',
+      name: 'name',
+      save: vi.fn(),
+      active: true
+    });
+    const url = await urlRepository.getUrlByShortId('shortId');
+    expect(url).instanceOf(Url);
+    expect(url).toEqual({
+      id: 'urlId',
+      shortId: 'shortId',
+      originalUrl: 'originalUrl',
+      user: 'userId',
+      name: 'name'
     });
   });
 
