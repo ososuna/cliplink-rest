035b6d987f391d7fa39770447306c7af4b2d9491
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2025-03-31 17:40:03 -0700

    test: add unit tests for user login in auth controller

--- test/presentation/auth/controller.test.ts
+++ test/presentation/auth/controller.test.ts
@@ -1,6 +1,6 @@
 import { beforeAll, describe, expect, it, vi } from 'vitest';
 import { Request, Response } from 'express';
-import { RegisterUser } from '@/domain';
+import { LoginUser, RegisterUser } from '@/domain';
 import { AuthDataSourceImpl } from '@/infrastructure';
 import { AuthController } from '@/presentation/auth/controller';
 import { AuthDataSourceMocks, createMockRequest, createMockResponse } from '@test/test-utils';
@@ -55,7 +55,7 @@ describe('auth controller', () => {
       );      
     });
 
-    it('should return 400 if dto is invalid', async () => {
+    it('should return error 400 if dto is invalid', async () => {
       const req = createMockRequest({
         body: {
           name: '',
@@ -75,5 +75,61 @@ describe('auth controller', () => {
       expect(res.json).toHaveBeenCalledWith({ error: expect.any(String) });
     });
 
+  });
+
+  describe('login user', () => {
+    it('should login user', async () => {
+      const mockUser = AuthDataSourceMocks.user;
+      vi.spyOn(LoginUser.prototype, 'execute').mockResolvedValue({
+        token: 'token',
+        user: {
+          id: mockUser.id,
+          name: mockUser.name,
+          lastName: mockUser.lastName,
+          email: mockUser.email
+        }
+      });
+      const req = createMockRequest({
+        body: {
+          email: 'john@test.com',
+          password: 'password'
+        },
+        method: 'POST',
+        url: '/auth/login'
+      });
+      const res = createMockResponse();
+      authController.loginUser(req as Request, res as Response);
+
+      await new Promise(process.nextTick);
+
+      expect(LoginUser.prototype.execute).toHaveBeenCalledTimes(1);
+      expect(LoginUser.prototype.execute).toHaveBeenCalledWith({
+        email: 'john@test.com',
+        password: 'password'
+      });
+      expect(res.cookie).toHaveBeenCalledWith(
+        'access_token',
+        'token',
+        expect.any(Object)
+      );      
+    });
+
+    it('should return error 400 if dto is invalid', async () => {
+      const req = createMockRequest({
+        body: {
+          email: '',
+          password: 'password'
+        },
+        method: 'POST',
+        url: '/auth/login'
+      });
+      const res = createMockResponse();
+      authController.loginUser(req as Request, res as Response);
+
+      await new Promise(process.nextTick);
+
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.json).toHaveBeenCalledWith({ error: expect.any(String) });
+    });
   });
 });
