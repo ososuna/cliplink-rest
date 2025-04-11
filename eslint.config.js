fe6f3daa0793d1c33d020aa8f116a0819917fe44
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2025-04-10 18:54:01 -0700

    chore: add linting and formatting

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import eslint from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tseslintParser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  eslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslintParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      'prettier': prettier,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...prettierConfig.rules,
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'prettier/prettier': 'error',
+    },
+  },
+  {
+    ignores: ['node_modules/**', 'dist/**', 'coverage/**', '*.js', '!jest.config.js'],
+  },
+]; 
